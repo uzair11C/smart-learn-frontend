d3755b42fdfdb8e792296afdb1bf0c90d106d938
Uzair Abdullah <uzairabdullah973@gmail.com>
2024-04-24 20:55:39 +0500

    Update RoadmapsList.js

--- src/components/RoadmapComponents/RoadmapsList.js
+++ src/components/RoadmapComponents/RoadmapsList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Box, Typography } from "@mui/material";
+import { Box, TextField, Typography } from "@mui/material";
 
 const RoadmapsList = () => {
     return (
@@ -17,8 +17,53 @@ const RoadmapsList = () => {
                 AI Generated Roadmaps
             </Typography>
 
-            <Box sx={{ width: "85%", background: "red", p: "15px" }}>
-                Section for roadmaps
+            <Box
+                sx={{
+                    width: "85%",
+                    p: "15px",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "stretch",
+                }}
+            >
+                <Typography variant="h5" textAlign="center">
+                    Our AI will generate upto date roadmaps for the year 2024,
+                    just search for them here
+                </Typography>
+                <TextField
+                    id="email"
+                    label="Search for a roadmap"
+                    variant="outlined"
+                    type="text"
+                    sx={{
+                        mt: "20px",
+                        ".MuiOutlinedInput-notchedOutline": {
+                            borderColor: "rgba(255,255,255,0.3)",
+                            borderRadius: "8px",
+                        },
+                        // ".MuiOutlinedInput-root": {
+                        //     background: "#222141",
+                        // },
+                    }}
+                    InputLabelProps={{
+                        style: { color: "rgba(255,255,255,0.3)" },
+                    }}
+                    inputProps={{
+                        style: {
+                            color: "#FFFFFF",
+                        },
+                    }}
+                    InputProps={{
+                        style: {
+                            // "&.hover": {
+                            //     borderColor: "rgba(255,255,255,0.3)",
+                            //     borderRadius: "8px",
+                            // },
+                            background: "#222141",
+                        },
+                    }}
+                />
             </Box>
         </Box>
     );
