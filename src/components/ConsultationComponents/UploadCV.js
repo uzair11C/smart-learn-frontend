3a466d5f1ac91fc185b67b23343025bb05569e54
Moeed Rafi <moeedrafi95@gmail.com>
2024-05-03 15:01:22 +0500

    Update UploadCV.js

--- src/components/ConsultationComponents/UploadCV.js
+++ src/components/ConsultationComponents/UploadCV.js
@@ -22,82 +22,82 @@ const UploadCV = () => {
     setFile(droppedFile);
   };
 
-    return (
+  return (
+    <Box
+      sx={{
+        width: "100%",
+        maxWidth: "100%",
+        p: { xs: "25px", md: "45px" },
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center",
+        alignItems: "center",
+        gap: "30px",
+      }}
+    >
+      <Typography variant="h2" component="h2" sx={{ fontSize: "9vmin" }}>
+        Upload Your Resume
+      </Typography>
+      <Box
+        sx={{
+          background:
+            "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
+          borderRadius: "12px",
+          p: "2px",
+        }}
+      >
         <Box
-            sx={{
-                width: "100%",
-                maxWidth: "100%",
-                p: { xs: "25px", md: "45px" },
-                display: "flex",
-                flexDirection: "column",
-                justifyContent: "center",
-                alignItems: "center",
-                gap: "30px",
-            }}
+          width="70vw"
+          sx={{
+            background: "#19192F",
+            p: "30px",
+            borderRadius: "12px",
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            gap: "20px",
+          }}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
         >
-            <Typography variant="h2" component="h2" sx={{ fontSize: "9vmin" }}>
-                Upload Your Resume
-            </Typography>
-            <Box
-                sx={{
-                    background:
-                        "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
-                    borderRadius: "12px",
-                    p: "2px",
-                }}
+          <Typography variant="h4" component="h4">
+            Select or drop your resume here
+          </Typography>
+          <img src="/images/Upload.png" alt="drag-here" />
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "center",
+              alignItems: "center",
+              gap: "30px",
+              width: "40%",
+            }}
+          >
+            <label
+              htmlFor="browse"
+              style={{
+                textTransform: "none",
+                background:
+                  "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
+                borderRadius: "8px",
+                fontSize: "20px",
+                padding: "10px 35px",
+                cursor: "pointer",
+              }}
             >
-                <Box
-                    width="70vw"
-                    sx={{
-                        background: "#19192F",
-                        p: "30px",
-                        borderRadius: "12px",
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "center",
-                        gap: "20px",
-                    }}
-                    onDragOver={handleDragOver}
-                    onDrop={handleDrop}
-                >
-                    <Typography variant="h4" component="h4">
-                        Select or drop your resume here
-                    </Typography>
-                    <img src="/images/Upload.png" alt="drag-here" />
-                    <Box
-                        sx={{
-                            display: "flex",
-                            flexDirection: "row",
-                            justifyContent: "center",
-                            alignItems: "center",
-                            gap: "30px",
-                            width: "40%",
-                        }}
-                    >
-                        <label
-                            htmlFor="browse"
-                            style={{
-                                textTransform: "none",
-                                background:
-                                    "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
-                                borderRadius: "8px",
-                                fontSize: "20px",
-                                padding: "10px 35px",
-                                cursor: "pointer",
-                            }}
-                        >
-                            <input
-                                type="file"
-                                hidden
-                                id="browse"
-                                accept=".pdf,.docx,.txt"
-                                onChange={handleFileSelect}
-                                // multiple
-                            />
-                            Choose File
-                        </label>
-                        {/* <Button
+              <input
+                type="file"
+                hidden
+                id="browse"
+                accept=".pdf,.docx,.txt"
+                onChange={handleFileSelect}
+                // multiple
+              />
+              Choose File
+            </label>
+            {/* <Button
                             variant="contained"
                             sx={{
                                 textTransform: "none",
@@ -130,15 +130,15 @@ const UploadCV = () => {
                             />
                             Choose File
                         </Button> */}
-                        <Typography variant="p" component="p">
-                            {file.name ? file.name : `No File Chosen`}
-                        </Typography>
-                        {/* <p>{file ? file.name : "No File Chosen"}</p> */}
-                    </Box>
-                </Box>
-            </Box>
+            <Typography variant="p" component="p">
+              {file.name ? file.name : `No File Chosen`}
+            </Typography>
+            {/* <p>{file ? file.name : "No File Chosen"}</p> */}
+          </Box>
         </Box>
-    );
+      </Box>
+    </Box>
+  );
 };
 
 export default UploadCV;
