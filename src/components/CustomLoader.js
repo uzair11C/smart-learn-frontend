c8592ae0d0aa61d7419acb286d5ecbc34688af4d
Uzair Abdullah <uzairabdullah973@gmail.com>
2024-05-09 21:04:06 +0500

    more loading messages

--- src/components/CustomLoader.js
+++ src/components/CustomLoader.js
@@ -3,13 +3,19 @@ import { Backdrop, CircularProgress, Typography } from "@mui/material";
 
 const CustomLoader = ({ open }) => {
     const texts = [
-        "Please wait, our AI is Working it's Magic!",
-        "Our AI is working, please be patient",
-        "Still loading, almost there!",
+        "Just a sec, our AI is untangling its virtual shoelaces.",
+        "Wait a moment, our AI is channeling its inner zen master.",
+        "Hold your horses, our AI is enjoying a virtual roller coaster ride!",
         "Hol' up, our AI is cookin'!",
+        "Almost there, our AI is untangling its digital spaghetti.",
+        "Hold tight, our AI is brewing up some magic!",
+        "Hold onto your hats, our AI is riding the virtual roller coaster of creativity!",
+        "Just a moment, our AI is solving the virtual Rubik's cube.",
+        "Hold your breath, our AI is diving into the digital deep!",
+        "Stay tuned, our AI is composing a digital symphony!",
     ];
 
-    const [text, setText] = useState("Please wait, our is Working it's Magic!");
+    const [text, setText] = useState(Math.floor(Math.random() * texts.length));
 
     function setRandomText() {
         const index = Math.floor(Math.random() * texts.length);
