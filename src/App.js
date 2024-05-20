d4c116a0700e3f4f151aa421988529e00b27cf2c
Uzair Abdullah <uzairabdullah973@gmail.com>
2024-05-20 08:34:07 +0500

    Revert "Authentication Supabase"
    This reverts commit 7786514b76a59d0d4d06856fffcb5dcbcda3c93d.

--- src/App.js
+++ src/App.js
@@ -5,40 +5,41 @@ import Home from "./pages/Home";
 import Consultation from "./pages/Consultation";
 import Roadmaps from "./pages/Roadmaps";
 import RolePrediction from "./pages/RolePrediction";
+import { PredictionContext } from "./Contexts/PredictionContext";
+import { useState } from "react";
 // import Result from "./pages/Result";
-// import { PredictionContext } from "./Contexts/PredictionContext";
 
 function App() {
-  // const [prediction, setPrediction] = useState({
-  //     majorRole: "",
-  //     otherRole1: "",
-  //     otherRole2: "",
-  // });
-  return (
-    // <PredictionContext.Provider value={[prediction, setPrediction]}>
-    <div
-      style={{
-        backgroundColor: "#19192F",
-        color: "#FFFFFF",
-        width: "100vw",
-        maxWidth: "100%",
-      }}
-    >
-      <CssBaseline />
-      <NavBar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/consultation" element={<Consultation />} />
-        <Route path="/resume-analysis" element={<RolePrediction />} />
-        {/* <Route
+    // const [prediction, setPrediction] = useState({
+    //     majorRole: "",
+    //     otherRole1: "",
+    //     otherRole2: "",
+    // });
+    return (
+        // <PredictionContext.Provider value={[prediction, setPrediction]}>
+        <div
+            style={{
+                backgroundColor: "#19192F",
+                color: "#FFFFFF",
+                width: "100vw",
+                maxWidth: "100%",
+            }}
+        >
+            <CssBaseline />
+            <NavBar />
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/consultation" element={<Consultation />} />
+                <Route path="/resume-analysis" element={<RolePrediction />} />
+                {/* <Route
                     path="/role-prediction/prediction"
                     element={<Result />}
                 /> */}
-        <Route path="/roadmaps" element={<Roadmaps />} />
-      </Routes>
-    </div>
-    // </PredictionContext.Provider>
-  );
+                <Route path="/roadmaps" element={<Roadmaps />} />
+            </Routes>
+        </div>
+        // </PredictionContext.Provider>
+    );
 }
 
 export default App;
--- src/components/NavBar/DesktopNav.js
+++ src/components/NavBar/DesktopNav.js
@@ -1,126 +1,127 @@
 import React from "react";
-import { Box, Typography, Button } from "@mui/material";
+import {
+    Box,
+    Typography,
+    // Button
+} from "@mui/material";
 import { Link } from "react-router-dom";
-import { useUser } from "../../Contexts/useUser";
 
 const DesktopNav = ({ handleCloseNavMenu }) => {
-  const { user, logout } = useUser();
-
-  return (
-    <Box
-      sx={{
-        width: "100%",
-        display: { xs: "none", md: "flex" },
-        flexDirection: "row",
-        justifyContent: "space-between",
-        alignItems: "center",
-      }}
-    >
-      <img
-        src="/images/SmartLearn_Logo.png"
-        alt="logo"
-        style={{
-          mr: 2,
-          display: { xs: "none", md: "flex" },
-        }}
-        width="250"
-      />
-      <Box
-        sx={{
-          width: "70%",
-          display: { xs: "none", md: "flex" },
-          justifyContent: "center",
-        }}
-      >
+    return (
         <Box
-          sx={{
-            pl: "10px",
-            pr: "30px",
-            height: "90px",
-            display: "flex",
-            flexDirection: "row",
-            justifyContent: "center",
-            alignItems: "center",
-            gap: "40px",
-          }}
+            sx={{
+                width: "100%",
+                display: { xs: "none", md: "flex" },
+                flexDirection: "row",
+                justifyContent: "space-between",
+                alignItems: "center",
+            }}
         >
-          <Typography
-            variant="h6"
-            component="h6"
-            onClick={handleCloseNavMenu}
-            sx={{
-              my: 2,
-              display: "block",
-              margin: "10px",
-              textDecoration: "none",
-            }}
-          >
-            <Link
-              style={{
-                textDecoration: "none",
-                color: "inherit",
-              }}
-              to="/"
+            <img
+                src="/images/SmartLearn_Logo.png"
+                alt="logo"
+                style={{
+                    mr: 2,
+                    display: { xs: "none", md: "flex" },
+                }}
+                width="250"
+            />
+            <Box
+                sx={{
+                    width: "70%",
+                    display: { xs: "none", md: "flex" },
+                    justifyContent: "center",
+                }}
             >
-              Home
-            </Link>
-          </Typography>
+                <Box
+                    sx={{
+                        pl: "10px",
+                        pr: "30px",
+                        height: "90px",
+                        display: "flex",
+                        flexDirection: "row",
+                        justifyContent: "center",
+                        alignItems: "center",
+                        gap: "40px",
+                    }}
+                >
+                    <Typography
+                        variant="h6"
+                        component="h6"
+                        onClick={handleCloseNavMenu}
+                        sx={{
+                            my: 2,
+                            display: "block",
+                            margin: "10px",
+                            textDecoration: "none",
+                        }}
+                    >
+                        <Link
+                            style={{
+                                textDecoration: "none",
+                                color: "inherit",
+                            }}
+                            to="/"
+                        >
+                            Home
+                        </Link>
+                    </Typography>
 
-          <Typography
-            variant="h6"
-            sx={{
-              display: "block",
-              margin: "10px",
-              textDecoration: "none",
-            }}
-          >
-            <Link
-              style={{
-                textDecoration: "none",
-                color: "#FFFFFF",
-              }}
-              to="/consultation"
-            >
-              Consultation
-            </Link>
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              display: "block",
-              margin: "10px",
-              textDecoration: "none",
-            }}
-          >
-            <Link
-              style={{
-                textDecoration: "none",
-                color: "#FFFFFF",
-              }}
-              to="/resume-analysis"
-            >
-              Resume Analysis
-            </Link>
-          </Typography>
-          <Typography
-            variant="h6"
-            sx={{
-              display: "block",
-              margin: "10px",
-              textDecoration: "none",
-            }}
-          >
-            <Link
-              style={{
-                textDecoration: "none",
-                color: "#FFFFFF",
-              }}
-              to="/roadmaps"
-            >
-              Roadmaps
-            </Link>
-          </Typography>
-          {/* 
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            display: "block",
+                            margin: "10px",
+                            textDecoration: "none",
+                        }}
+                    >
+                        <Link
+                            style={{
+                                textDecoration: "none",
+                                color: "#FFFFFF",
+                            }}
+                            to="/consultation"
+                        >
+                            Consultation
+                        </Link>
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            display: "block",
+                            margin: "10px",
+                            textDecoration: "none",
+                        }}
+                    >
+                        <Link
+                            style={{
+                                textDecoration: "none",
+                                color: "#FFFFFF",
+                            }}
+                            to="/resume-analysis"
+                        >
+                            Resume Analysis
+                        </Link>
+                    </Typography>
+                    <Typography
+                        variant="h6"
+                        sx={{
+                            display: "block",
+                            margin: "10px",
+                            textDecoration: "none",
+                        }}
+                    >
+                        <Link
+                            style={{
+                                textDecoration: "none",
+                                color: "#FFFFFF",
+                            }}
+                            to="/roadmaps"
+                        >
+                            Roadmaps
+                        </Link>
+                    </Typography>
+                    {/* 
                     <Typography
                         variant="h6"
                         component="h6"
@@ -141,69 +142,25 @@ const DesktopNav = ({ handleCloseNavMenu }) => {
                         >
                             Profile
                         </Link>
-                    </Typography>*/}
-
-          {user?.id && (
-            <Link to="/profile">
-              <Box
-                component="img"
-                sx={{
-                  height: 50,
-                  width: 50,
-                  borderRadius: "50px",
-                }}
-                alt={`${user?.user_metadata.full_name} Photo`}
-                src={
-                  user?.user_metadata.avatar_url || "/images/placeholder.jpg"
-                }
-              />
-            </Link>
-          )}
-
-          {user?.id ? (
-            <Button
-              variant="contained"
-              onClick={logout}
-              sx={{
-                textTransform: "none",
-                background:
-                  "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
-                borderRadius: "8px",
-                fontSize: "20px",
-                pl: "30px",
-                pr: "30px",
-              }}
-            >
-              LogOut
-            </Button>
-          ) : (
-            <Button
-              variant="contained"
-              sx={{
-                textTransform: "none",
-                background:
-                  "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
-                borderRadius: "8px",
-                fontSize: "20px",
-                pl: "30px",
-                pr: "30px",
-              }}
-            >
-              <Link
-                to="/sign-up"
-                style={{
-                  textDecoration: "none",
-                  color: "inherit",
-                }}
-              >
-                Sign Up
-              </Link>
-            </Button>
-          )}
+                    </Typography>
+                    <Button
+                        variant="contained"
+                        sx={{
+                            textTransform: "none",
+                            background:
+                                "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
+                            borderRadius: "8px",
+                            fontSize: "20px",
+                            pl: "30px",
+                            pr: "30px",
+                        }}
+                    >
+                        Logout
+                    </Button> */}
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default DesktopNav;
