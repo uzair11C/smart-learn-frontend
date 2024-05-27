c48402539c1114211930a6fb9e9b2e0f78bdfe1b
Uzair Abdullah <uzairabdullah973@gmail.com>
2024-05-27 21:41:12 +0500

    made responsive cv, jobs

--- src/components/RolePredictionComponents/JobAnalysis.js
+++ src/components/RolePredictionComponents/JobAnalysis.js
@@ -2,83 +2,89 @@ import { Box, Typography } from "@mui/material";
 import React from "react";
 
 const JobAnalysis = ({ majorRole, JobAnalysis }) => {
-  return (
-    <Box
-      sx={{
-        width: "75%",
-        p: "20px",
-        display: "flex",
-        flexDirection: "column",
-        gap: "20px",
-      }}
-    >
-      <Typography
-        variant="h2"
-        component="h3"
-        textAlign="center"
-        fontWeight="bold"
-        sx={{ mb: "20px" }}
-      >
-        Job Analysis for {majorRole}
-      </Typography>
-
-      <Box>
-        <Typography
-          variant="h4"
-          component="h3"
-          fontWeight="bold"
-          sx={{ textDecoration: "underline" }}
+    return (
+        <Box
+            sx={{
+                width: "75%",
+                p: "20px",
+                display: "flex",
+                flexDirection: "column",
+                gap: "20px",
+            }}
         >
-          Demand:
-        </Typography>
-        <Typography variant="h6">
-          {JobAnalysis && JobAnalysis.demand}
-        </Typography>
-      </Box>
+            <Typography
+                variant="h2"
+                component="h3"
+                textAlign="center"
+                fontWeight="bold"
+                fontSize="8vmin"
+                sx={{ mb: "20px" }}
+            >
+                Job Analysis for {majorRole}
+            </Typography>
 
-      <Box>
-        <Typography
-          variant="h4"
-          component="h3"
-          fontWeight="bold"
-          sx={{ textDecoration: "underline" }}
-        >
-          Key Skills and Technologies:
-        </Typography>
-        <Typography variant="h6">
-          {JobAnalysis && JobAnalysis.keySkillsAndTechnologies.join(", ")}
-        </Typography>
-      </Box>
+            <Box>
+                <Typography
+                    variant="h4"
+                    component="h3"
+                    fontWeight="bold"
+                    fontSize="6vmin"
+                    sx={{ textDecoration: "underline" }}
+                >
+                    Demand:
+                </Typography>
+                <Typography variant="h6" fontSize="5vmin">
+                    {JobAnalysis && JobAnalysis.demand}
+                </Typography>
+            </Box>
 
-      <Box>
-        <Typography
-          variant="h4"
-          component="h3"
-          fontWeight="bold"
-          sx={{ textDecoration: "underline" }}
-        >
-          Job opportunities:
-        </Typography>
-        <Typography variant="h6">
-          {JobAnalysis && JobAnalysis.jobOpportunities.join(", ")}
-        </Typography>
-      </Box>
+            <Box>
+                <Typography
+                    variant="h4"
+                    component="h3"
+                    fontWeight="bold"
+                    fontSize="6vmin"
+                    sx={{ textDecoration: "underline" }}
+                >
+                    Key Skills and Technologies:
+                </Typography>
+                <Typography variant="h6" fontSize="5vmin">
+                    {JobAnalysis &&
+                        JobAnalysis.keySkillsAndTechnologies.join(", ")}
+                </Typography>
+            </Box>
 
-      <Box>
-        <Typography
-          variant="h4"
-          component="h3"
-          fontWeight="bold"
-          sx={{ textDecoration: "underline" }}
-        >
-          Salaries Offered:
-        </Typography>
-        <Typography variant="h6">
-          {JobAnalysis && JobAnalysis.salariesOffered} /-
-        </Typography>
-      </Box>
-    </Box>
-  );
+            <Box>
+                <Typography
+                    variant="h4"
+                    component="h3"
+                    fontWeight="bold"
+                    fontSize="6vmin"
+                    sx={{ textDecoration: "underline" }}
+                >
+                    Job opportunities:
+                </Typography>
+                <Typography variant="h6" fontSize="5vmin">
+                    {JobAnalysis && JobAnalysis.jobOpportunities.join(", ")}
+                </Typography>
+            </Box>
+
+            <Box>
+                <Typography
+                    variant="h4"
+                    component="h3"
+                    fontWeight="bold"
+                    fontSize="6vmin"
+                    sx={{ textDecoration: "underline" }}
+                >
+                    Salaries Offered:
+                </Typography>
+                <Typography variant="h6" fontSize="5vmin">
+                    {JobAnalysis && JobAnalysis.salariesOffered} /-
+                </Typography>
+            </Box>
+        </Box>
+    );
 };
 
 export default JobAnalysis;
--- src/components/RolePredictionComponents/JobsListing.js
+++ src/components/RolePredictionComponents/JobsListing.js
@@ -3,131 +3,145 @@ import React from "react";
 import CardComponent from "../CardComponent";
 
 const JobsListing = ({ majorRole, jobs }) => {
-  return (
-    <Box
-      sx={{
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-        width: "85%",
-        maxWidth: "100%",
-      }}
-    >
-      <Box
-        sx={{
-          width: "85%",
-          maxWidth: "100%",
-          p: "2%",
-          display: "flex",
-          flexDirection: "column",
-          justifyContent: "center",
-          alignItems: "center",
-        }}
-      >
-        <Typography
-          variant="h2"
-          component="h3"
-          textAlign="center"
-          fontWeight="bold"
-          sx={{ mb: "20px", width: "100%" }}
-        >
-          Jobs for {majorRole}
-        </Typography>
+    return (
         <Box
-          sx={{
-            width: "100%",
-            p: "2%",
-          }}
+            sx={{
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "center",
+                alignItems: "center",
+                width: { xs: "100%", md: "85%" },
+                maxWidth: "100%",
+            }}
         >
-          <Grid container spacing={3}>
-            {jobs &&
-              jobs.map((jobData) => (
-                <Grid item xs={12} key={jobData.job_id}>
-                  <CardComponent
-                    borderRadius="12px"
-                    padding="5%"
-                    minWidth="20%"
-                    maxWidth="50%"
-                    height="50%"
+            <Box
+                sx={{
+                    width: "85%",
+                    maxWidth: "100%",
+                    p: "2%",
+                    display: "flex",
+                    flexDirection: "column",
+                    justifyContent: "center",
+                    alignItems: "center",
+                }}
+            >
+                <Typography
+                    variant="h2"
+                    component="h3"
+                    textAlign="center"
+                    fontWeight="bold"
+                    fontSize="9vmin"
+                    sx={{ mb: "20px", width: "100%" }}
+                >
+                    Jobs for {majorRole}
+                </Typography>
+                <Box
                     sx={{
-                      display: "flex",
-                      flexDirection: "row",
-                      justifyContent: "space-between",
-                      alignItems: "center",
-                      gap: "20px",
-                      transition: "transform 0.3s ease", // Add transition property
-                      ":hover": {
-                        transform: "scale(1.05)", // Scale up on hover
-                      },
+                        width: "100%",
+                        p: "2%",
                     }}
-                  >
-                    <Avatar
-                      alt={jobData.employer_name}
-                      src={jobData.employer_logo}
-                      variant="rounded"
-                      sx={{
-                        width: 225,
-                        height: 225,
-                        borderRadius: "12px",
-                      }}
-                    />
-                    <Box
-                      sx={{
-                        display: "flex",
-                        flexDirection: "column",
-                        justifyContent: "center",
-                        alignItems: "flex-start",
-                      }}
-                    >
-                      <Typography variant="h4" fontWeight="bold">
-                        {jobData.job_title}
-                      </Typography>
-                      <Typography variant="h5">
-                        {jobData.employer_name}
-                      </Typography>
-                      <Typography
-                        variant="body1"
-                        sx={{
-                          overflow: "hidden",
-                          display: "-webkit-box",
-                          WebkitLineClamp: 2,
-                          WebkitBoxOrient: "vertical",
-                          maxHeight: "3.6em",
-                          width: "90%",
-                          mt: "10px",
-                          mb: "20px",
-                        }}
-                      >
-                        {jobData.job_description}
-                      </Typography>
-                      <Button
-                        variant="contained"
-                        sx={{
-                          textTransform: "none",
-                          background:
-                            "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
-                          borderRadius: "8px",
-                          fontSize: "20px",
-                          padding: "10px 30px",
-                        }}
-                        onClick={() =>
-                          window.open(jobData.job_apply_link, "_blank")
-                        }
-                      >
-                        Apply Now
-                      </Button>
-                    </Box>
-                  </CardComponent>
-                </Grid>
-              ))}
+                >
+                    <Grid container spacing={3}>
+                        {jobs &&
+                            jobs.map((jobData) => (
+                                <Grid item xs={12} key={jobData.job_id}>
+                                    <CardComponent
+                                        borderRadius="12px"
+                                        padding="5%"
+                                        minWidth="20%"
+                                        maxWidth="50%"
+                                        minHeight="50%"
+                                        sx={{
+                                            display: "flex",
+                                            flexDirection: {
+                                                xs: "column",
+                                                md: "row",
+                                            },
+                                            justifyContent: "space-between",
+                                            alignItems: "center",
+                                            gap: "20px",
+                                            transition: "transform 0.3s ease", // Add transition property
+                                            ":hover": {
+                                                transform: "scale(1.05)", // Scale up on hover
+                                            },
+                                        }}
+                                    >
+                                        <Avatar
+                                            alt={jobData.employer_name}
+                                            src={jobData.employer_logo}
+                                            variant="rounded"
+                                            sx={{
+                                                width: 225,
+                                                height: 225,
+                                                borderRadius: "12px",
+                                            }}
+                                        />
+                                        <Box
+                                            sx={{
+                                                display: "flex",
+                                                flexDirection: "column",
+                                                justifyContent: "center",
+                                                alignItems: {
+                                                    xs: "stretch",
+                                                    md: "flex-start",
+                                                },
+                                            }}
+                                        >
+                                            <Typography
+                                                variant="h4"
+                                                fontWeight="bold"
+                                                fontSize="7vmin"
+                                            >
+                                                {jobData.job_title}
+                                            </Typography>
+                                            <Typography variant="h5">
+                                                {jobData.employer_name}
+                                            </Typography>
+                                            <Typography
+                                                variant="body1"
+                                                sx={{
+                                                    overflow: "hidden",
+                                                    display: "-webkit-box",
+                                                    WebkitLineClamp: 2,
+                                                    WebkitBoxOrient: "vertical",
+                                                    maxHeight: "3.6em",
+                                                    width: "90%",
+                                                    mt: "10px",
+                                                    mb: "20px",
+                                                }}
+                                            >
+                                                {jobData.job_description}
+                                            </Typography>
+                                            <Button
+                                                variant="contained"
+                                                sx={{
+                                                    textTransform: "none",
+                                                    background:
+                                                        "linear-gradient(108.51deg, #F219A1 53.69%, #AD0CF8 100.22%, #FE007E 100.23%)",
+                                                    borderRadius: "8px",
+                                                    fontSize: "20px",
+                                                    padding: "10px 30px",
+                                                }}
+                                                onClick={() =>
+                                                    window.open(
+                                                        jobData.job_apply_link,
+                                                        "_blank"
+                                                    )
+                                                }
+                                            >
+                                                Apply Now
+                                            </Button>
+                                        </Box>
+                                    </CardComponent>
+                                </Grid>
+                            ))}
 
-            {/* ))} */}
-          </Grid>
+                        {/* ))} */}
+                    </Grid>
+                </Box>
+            </Box>
         </Box>
-      </Box>
-    </Box>
-  );
+    );
 };
 
 export default JobsListing;
